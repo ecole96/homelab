c63eccc2c029ed99d8800e4af30af7e59200e514
Evan Cole <ecole96@outlook.com>
2026-01-10 12:49:20 -0500

    delete files

--- restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run as root cronjob
-# to add crontab: crontab /media-homeserver/crontab
-
-docker compose -f /media-homeserver/prod.yml down
-docker compose -f /media-homeserver/prod.yml up -d
